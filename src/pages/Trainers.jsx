1d2c17f74dda6bb73be702656c22bc53f3916723
newmorin2 <newton.mwangi@student.moringaschool.com>
2026-07-22 23:25:22 +0300

    adding CRUD functionality

--- src/pages/Trainers.jsx
+++ src/pages/Trainers.jsx
@@ -1,4 +1,64 @@
+import { useEffect, useState } from "react";
+import api from "../api/axios";
+
+
 function Trainers(){
+
+    const [form,setForm] = useState({
+        name:"",
+        email:"",
+        specialization:"",
+        experience:""
+    });
+
+    const getTrainers = ()=>{
+
+        api.get("trainers/")
+        .then(response=>{
+            setTrainers(response.data);
+        });
+    };
+
+    useEffect(()=>{
+        getTrainers();
+    },[]);
+
+    const handleChange = (e)=>{
+
+        setForm({
+
+            ...form,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const addTrainer = (e)=>{
+
+        e.preventDefault();
+        api.post(
+            "trainers/",
+            form
+        )
+        .then(()=>{
+
+            setForm({
+                name:"",
+                email:"",
+                specialization:"",
+                experience:""
+            });
+
+            getTrainers();
+        });
+    };
+
+    const deleteTrainer = (id)=>{
+
+        api.delete(`trainers/${id}/`)
+        .then(()=>{
+            getTrainers();
+        });
+    };
 
     return(
 
@@ -7,6 +67,92 @@ function Trainers(){
             <h1>
                 Trainers
             </h1>
+
+            <form onSubmit={addTrainer}>
+
+                <input
+                    name="name"
+                    placeholder="Trainer Name"
+                    value={form.name}
+                    onChange={handleChange}
+                />
+
+                <input
+                    name="email"
+                    placeholder="Email"
+                    value={form.email}
+                    onChange={handleChange}
+                />
+
+                <input
+                    name="specialization"
+                    placeholder="Specialization"
+                    value={form.specialization}
+                    onChange={handleChange}
+                />
+
+                <input
+                    name="experience"
+                    placeholder="Experience (years)"
+                    value={form.experience}
+                    onChange={handleChange}
+                />
+
+                <button>
+                    Add Trainer
+                </button>
+            </form>
+
+            <table>
+
+                <thead>
+
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Specialization</th>
+                        <th>Experience</th>
+                        <th>Action</th>
+                    </tr>
+
+                </thead>
+
+                <tbody>
+
+                {
+                    trainers.map(trainer=>(
+
+                        <tr key={trainer.id}>
+
+                            <td>
+                                {trainer.name}
+                            </td>
+
+                            <td>
+                                {trainer.email}
+                            </td>
+
+                            <td>
+                                {trainer.specialization}
+                            </td>
+
+                            <td>
+                                {trainer.experience} years
+                            </td>
+
+                            <td>
+                                <button
+                                onClick={()=>deleteTrainer(trainer.id)}
+                                >
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+
+                    ))
+                }
+                </tbody>
+            </table>
 
         </div>
 
